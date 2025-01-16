87d8fa40ea18480e6d1d53739d56dc49db69d700
lecol <alves.colin@icloud.com>
2025-01-16 23:23:23 +0100

    First commit

---
+++ assets/views/AboutView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h3>C'est la about view</h3>
+</template>
+
+<style scoped>
+
+</style>
---
+++ assets/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h3>C'est la home view</h3>
+</template>
